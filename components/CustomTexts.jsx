6366f855cac18fca51ba85fcd9bfe0108f912c3f
Chimezie Oji <chimezieoji@yahoo.com>
2023-01-14 13:13:20 +0100

    bug

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion';
 
-import { textContainer, textVariant2 } from "../utils/motion";
+import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p 
@@ -25,5 +25,7 @@ export const TitleText = ({ title, textStyles }) => (
     whileInView="show"
     className={`mt-[-8px] font-bold md:text-[64px] text-[[40px]
     text-white ${textStyles}`}
-  >{title}</motion.h2>
+  >
+    {title}
+  </motion.h2>
 );
